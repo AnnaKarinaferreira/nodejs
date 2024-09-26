2314958b091ae2df0b773dd2c764d0bddda8fac5
annapacheco <annapacheco203@gmail.com>
2024-09-26 09:54:29 -0300

    aaaa

--- express-aula1/index.js
+++ express-aula1/index.js
@@ -4,6 +4,16 @@ const port = 3000 //variavel ambiente
 
 const path = require("path")
 const basepath =  path.join(__dirname, 'Aula1') //dirname é uma variavel para indicar o caminho
+const checkAuth = function(req,res,next){
+    req.authStatus = true 
+    if(req.authStatus){
+        console.log('Está logado!')
+    } else {
+        console.log('Não está logado, faça login!')
+    }
+}
+app.use(checkAuth)
+
 app.get('/', (req, res) =>{
 res.sendFile(`${basepath}/index.html`)
 })
