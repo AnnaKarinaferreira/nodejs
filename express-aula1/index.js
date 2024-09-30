60f3f757d28d5c12c247621d7cc884605a2d0b66
annakarina <annapacheco203@gmail.com>
2024-09-30 12:25:13 -0300

    up

--- express-aula1/index.js
+++ express-aula1/index.js
@@ -1,9 +1,20 @@
 const express =  require('express')
 const app = express()
-const port = 3000 //variavel ambiente
+//variavel ambiente
+const port = 3000
 
 const path = require("path")
-const basepath =  path.join(__dirname, 'Aula1') //dirname é uma variavel para indicar o caminho
+
+//dirname é uma variavel para indicar o caminho
+const basepath =  path.join(__dirname) 
+
+app.use(
+    express.urlencoded({ //converter para formar url
+extended: true, //copiar parametros dos objetos filhos
+    })
+)
+app.use(express.json())
+
 const checkAuth = function(req,res,next){
     req.authStatus = true 
     if(req.authStatus){
@@ -13,6 +24,29 @@ const checkAuth = function(req,res,next){
     }
 }
 app.use(checkAuth)
+app.get('users/add', (req, res)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  => {
+    res.sendFile(`${basepath}/forms.html`)
+})
+app.post('/users/save', (req, res) => {
+    console.log(req.body)
+    const name = req.body.name
+    const email = req.body.email
+
+    console.log(name)
+    console.log(email)
+})
+//retorna um valor
+app.get('/users/:id', (req, res) => {
+
+    //resgatar parametros do url
+    const id = req.params.id
+
+    //resgatar o usuario do banco
+    console.log(`Estamos buscando o usuario: $(id)`) 
+
+    //senfile:trasferir arquivos//basepath: especifica a pasta raiz
+    res.sendFile(`${basepath}/users.html`) 
+})
 
 app.get('/', (req, res) =>{
 res.sendFile(`${basepath}/index.html`)
